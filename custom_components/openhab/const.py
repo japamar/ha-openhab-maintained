9cd7c368b8de5f243a9505e1ec80573700dfc849
Jaime <japamar@gmail.com>
2026-09-05 00:26:49 +0100

    Add maintained integration metadata and API client

---
+++ custom_components/openhab/const.py
@@ -0,0 +1,66 @@
+"""Constants for openHAB."""
+from datetime import timedelta
+from logging import Logger, getLogger
+
+NAME = "openHAB"
+DOMAIN = "openhab"
+DOMAIN_DATA = f"{DOMAIN}_data"
+VERSION = "0.1.0"
+ATTRIBUTION = "Data provided by openHAB REST API"
+ISSUE_URL = "https://github.com/japamar/ha-openhab-maintained/issues"
+DATA_COORDINATOR_UPDATE_INTERVAL = timedelta(seconds=15)
+LOGGER: Logger = getLogger(__package__)
+
+BINARY_SENSOR = "binary_sensor"
+CAMERA = "camera"
+COVER = "cover"
+DEVICE_TRACKER = "device_tracker"
+LIGHT = "light"
+MEDIA_PLAYER = "media_player"
+SENSOR = "sensor"
+SWITCH = "switch"
+PLATFORMS = [BINARY_SENSOR, COVER, DEVICE_TRACKER, LIGHT, MEDIA_PLAYER, SENSOR, SWITCH]
+
+CONF_ENABLED = "enabled"
+CONF_BASE_URL = "base_url"
+CONF_AUTH_TYPE = "auth_type"
+CONF_USERNAME = "username"
+CONF_PASSWORD = "password"
+CONF_AUTH_TOKEN = "auth_token"
+CONF_AUTH_TYPE_BASIC = "basic"
+CONF_AUTH_TYPE_TOKEN = "token"
+
+AUTH_TYPES = [CONF_AUTH_TYPE_BASIC, CONF_AUTH_TYPE_TOKEN]
+DEFAULT_NAME = DOMAIN
+
+ITEMS_MAP = {
+    BINARY_SENSOR: ["Contact"],
+    CAMERA: ["Image"],
+    COVER: ["Rollershutter"],
+    DEVICE_TRACKER: ["Location"],
+    LIGHT: ["Color", "Dimmer"],
+    MEDIA_PLAYER: ["Player"],
+    SENSOR: [
+        "DateTime",
+        "Number",
+        "Number:Length",
+        "Number:Temperature",
+        "Number:Pressure",
+        "Number:Speed",
+        "Number:Intensity",
+        "Number:Dimensionless",
+        "Number:Angle",
+        "String",
+    ],
+    SWITCH: ["Switch"],
+}
+
+STARTUP_MESSAGE = f"""
+-------------------------------------------------------------------
+{NAME}
+Version: {VERSION}
+This is a custom integration!
+If you have any issues, open an issue here:
+{ISSUE_URL}
+-------------------------------------------------------------------
+"""
